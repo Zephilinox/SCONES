e944118db5325ce214f68403a05cf464cccc2e89
Nathan <nathan.butt@gmail.com>
2020-10-18 21:58:51 +0100

    Created PPU skeleton.

--- src/NES/PPU.cpp
+++ src/NES/PPU.cpp
@@ -1,1 +1,12 @@
 #include "PPU.hpp"
+
+PPU::PPU(Bus* bus)
+    : addBus(bus)
+    , vram(std::make_unique<std::uint8_t[]>(PPU_MAX_ADDRESSABLE_MEMEORY))
+{
+}
+
+PPU::~PPU()
+{
+
+}
--- src/NES/PPU.hpp
+++ src/NES/PPU.hpp
@@ -1,8 +1,31 @@
 #pragma once
 
+#include <cstdint>
+#include <memory>
+
+class Bus;
+
+constexpr std::uint32_t PPU_MAX_ADDRESSABLE_MEMEORY = 16 * 1024;
+
+// PPU I/O Addresses
+constexpr std::uint8_t PPU_ADRESS_CONTROL_REG_1 = 0x2000;
+constexpr std::uint8_t PPU_ADRESS_CONTROL_REG_2 = 0x2001;
+constexpr std::uint8_t PPU_ADRESS_STATUS_REG = 0x2002;
+constexpr std::uint8_t PPU_ADRESS_SPR_RAM_ADDRESS_REG = 0x2003;
+constexpr std::uint8_t PPU_ADRESS_SPR_RAM_IO_REG = 0x2004;
+constexpr std::uint8_t PPU_ADRESS_VRAM_REG_1 = 0x2005;
+constexpr std::uint8_t PPU_ADRESS_VRAM_REG_2 = 0x2006;
+constexpr std::uint8_t PPU_ADRESS_VRAM_IO_REG = 0x2007;
+
 class PPU
 {
 public:
+    PPU(Bus* bus);
+    ~PPU(); 
+
+
 
 private:
+    Bus* addBus = nullptr;
+    std::unique_ptr<std::uint8_t[]> vram;
 };
