1e3fe4e5f3b21002a76b1a536503280674ab5e91
Nathan <nathan.butt@gmail.com>
2020-10-18 20:04:20 +0100

    Created Address bus.

--- src/NES/Bus.hpp
+++ src/NES/Bus.hpp
@@ -1,8 +1,20 @@
 #pragma once
 
+#include <cstdint>
+#include <memory>
+
+constexpr std::int32_t ADDRESS_BUS_RAM_SIZE = 64 * 1024; 
+
+
 class Bus
 {
 public:
+   Bus();
+  ~Bus() = default;
 
-private:
+  std::uint8_t read(std::uint16_t add);
+  void write(std::uint16_t add, std::uint8_t data);
+
+private: 
+	std::unique_ptr<std::uint8_t> ram;
 };
